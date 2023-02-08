ce9267d561d7ddef3d122ce33fc02da443d436c3
Cecilie Nielsen <c.nielsen1507@gmail.com>
2023-02-08 17:57:45 +0100

    category dynamic done

--- js/category.js
+++ js/category.js
@@ -1,21 +1,23 @@
-fetch("https://kea-alt-del.dk/t7/api/categories")
-    .then(res=>res.json())
-    .then(showCategories)
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("category");
+
+fetch("https://kea-alt-del.dk/t7/api/products?category="+category)
+    .then((res) => res.json())
+    .then(showCategories);
 
 function showCategories(cats){
-    cats.forEach(showCategory)
+    cats.forEach(showCategory);
 }
 
-function showCategory(car){
+function showCategory(cat){
     //fanger vores template
     const template = document.querySelector("template").content;
-
     //cloner
     const clone = template.cloneNode(true);
-
     //ændre indhold
-    clone.querySelector("a").textContent=cat.category;
+    clone.querySelector("a").textContent = cat.category;
+    clone.querySelector("a").href = `productlist.html=${cat.category}`;
 
     //appender
     document.querySelector(".letterGroup ol").appendChild(clone);
-}
+}    
